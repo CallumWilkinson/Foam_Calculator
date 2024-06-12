b4c0ed4a919402f62ef0273029402997f9011a93
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2024-06-12 12:19:56 +1000

    git init blank project with test project
    added main proj as dependancy to test proj

---
+++ Foam_Calculator.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Foam_Calculator.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
